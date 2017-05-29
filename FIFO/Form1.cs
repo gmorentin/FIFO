e2f51bddb3fffe350023e3d504a0166d0ebecc08
Gustavo Fernando Morentin ballesteros <gmorentin@ucol.mx>
2017-05-28 20:55:27 -0500

    Modificado

---
+++ FIFO/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFO
+{
+    public partial class Form1 : Form
+    {
+        Queue<Proceso> miFifo = new Queue<Proceso>();
+        Random alea = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComenzar_Click(object sender, EventArgs e)
+        {
+            int cpa = 0;
+            int ccu = 0;
+            for(int i = 0; i <= 200; i++)
+            {
+                if (alea.Next(1, 100) <= 25)
+                {
+                    Proceso nuevo = new Proceso(alea.Next(4, 14));
+                    miFifo.Enqueue(nuevo);
+                    txtProcesos.Text += i + "Proceso: " + alea+Environment.NewLine;
+                }
+                if (miFifo.Peek() != null)//count tiene que ser mayor a 0
+                {
+                    miFifo.Peek().tiempo--;
+                    if (miFifo.Peek().tiempo == 0)
+                    {
+                        cpa++;
+                        miFifo.Dequeue();
+                    }
+                }
+                else
+                    ccu++;
+                txtProcesos.Text += cpa+Environment.NewLine;
+                txtProcesos.Text += ccu + Environment.NewLine; ;
+            }
+        }
+
+    }
+}
---
+++ FIFO/Proceso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+    class Proceso
+    {
+        private int _tiempo;
+        public int tiempo { get { return _tiempo; } set { _tiempo = value; } }
+
+        public Proceso(int tiempo1)
+        {
+            tiempo1=_tiempo;
+        }
+    }
+}
